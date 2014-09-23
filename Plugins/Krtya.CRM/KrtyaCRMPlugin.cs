cce3adc4ce40823615042486038fe8f1a710af6a
Dharmik Bhandari <KRTYASOFTWARES\Dharmik.Bhandari>
2014-09-23 12:02:19 +0530

    CRM- Added Plugin Skeleton

---
+++ Plugins/Krtya.CRM/KrtyaCRMPlugin.cs
@@ -0,0 +1,52 @@
+﻿using Nop.Core.Plugins;
+using Nop.Web.Framework.Menu;
+using Nop.Web.Framework.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krtya.CRM
+{
+    class KrtyaCRMPlugin: BasePlugin, IAdminMenuPlugin
+    {
+        #region Fields
+
+        
+
+        #endregion
+
+        #region Ctor
+
+        
+        #endregion
+
+        #region Methods
+
+
+        public bool Authenticate()
+        {
+            return true;
+        }
+
+        public override void Install()
+        {
+            base.Install();
+        }
+
+        public override void Uninstall()
+        {
+            base.Uninstall();
+        }
+
+        #endregion
+
+
+        public Nop.Web.Framework.Menu.SiteMapNode BuildMenuItem()
+        {
+            SiteMapNode node = new SiteMapNode { Visible = true, Title = "Krtya CRM", Url = "/Plugins/KrtyaShopLicense/Configure" };
+            return node;
+        }
+    }
+}
