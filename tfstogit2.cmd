e58d9a367dad754a743e6708d1557e49acf456f8
dharmikbhandari <dharmik.bhandari@krtya.com>
2014-10-29 16:47:39 +0530

    initial commit after git-tf clone-1

--- tfstogit2.cmd
+++ tfstogit2.cmd
@@ -1,7 +1,7 @@
 git init
 REM from the root, add and commit any new files
-git add .
-git commit -a -m "initial commit after git-tf clone"
+git add a_text_file.txt 
+git commit -a -m "initial commit after git-tf clone-1"
 
 REM add my github remote
 git remote add origin https://github.com/krtyarepo/krtyaspeeduptest.git
